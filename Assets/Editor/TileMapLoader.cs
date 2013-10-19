86330c00b6f97f99bc57e56d101ee4eec44d580c
Kjell Otto <otto.kjell@gmail.com>
2013-10-19 18:39:52 +0200

    initial commit

---
+++ Assets/Editor/TileMapLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TileMap))]
+public class TileMapLoader : Editor {
+	
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+		DrawDefaultInspector();
+	}
+}
---
+++ Assets/TileMap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class TileMap : MonoBehaviour {
+	
+	List<TileLayer> layers = new List<TileLayer>();
+	List<TileSet> sets = new List<TileSet>();
+	
+	// Use this for initialization
+	void Start () {
+		//Load File
+		//Parse File
+		//foreach layer 
+		//foreach 
+		
+	}
+	
+	void LoadFile(){
+	}
+	
+	void ParseFile(){
+	}
+	
+	void GenerateLayers(){
+	}
+}
